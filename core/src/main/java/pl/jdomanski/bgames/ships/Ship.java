33846bdf075465bf2af298c340c96443392ea1e1
jarek <jarek@laptop>
2018-11-24 22:00:46 +0100

    add Logger

--- core/src/main/java/pl/jdomanski/bgames/ships/Ship.java
+++ core/src/main/java/pl/jdomanski/bgames/ships/Ship.java
@@ -2,11 +2,14 @@ package pl.jdomanski.bgames.ships;
 
 import java.util.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.jdomanski.bgames.Vector;
 
 public class Ship {
 
 	// == fields ==
+	private final Logger log = LoggerFactory.getLogger(Ship.class);
 	private Set<Cell> parts;
 	private boolean sunk;
 	private ShipTypes type;
@@ -18,7 +21,12 @@ public class Ship {
 		this.type = type;
 		this.sunk = false;
 
-		parts.forEach(cell -> cell.setShip(this));
+		log.info("Ship {} created. Parts: {}", this, this.parts);
+
+		this.parts.forEach(cell -> {
+			cell.setShip(this);
+			log.info("Cell for ship {}", cell.hashCode());
+		});
 	}
 	
 	// == public methods ==
@@ -27,8 +35,11 @@ public class Ship {
 	}
 
 	public void hit() {
-
-		sunk = parts.stream().allMatch(cell -> cell.isHit() );
-
+		log.info("hit ship -> {}", this);
+		log.info("parts {}", parts);
+		sunk = parts.stream().allMatch(cell -> {
+			log.info("Cell hash {} hit? {}", cell.hashCode(), cell.isHit());
+			return cell.isHit();
+		} );
 	}
 }
